8dbe346cc53cd3112ca9cc2d853f1364ba5548be
abhimanyutiwaribot <tiwariabhi2004@gmail.com>
2026-06-02 18:49:12 +0530

    starter files added

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { Box, Text } from "ink";
+import HomeScreen from "./screens/HomeScreen.js";
+import RequestScreen from "./screens/RequestScreen.js";
+import CollectionsScreen from "./screens/CollectionsScreen.js";
+import HistoryScreen from "./screens/HistoryScreen.js";
+
+type Screen = "home" | "request" | "collections" | "history";
+
+export default function App() {
+  const [currentScreen, setCurrentScreen] = useState<Screen>("home");
+
+  const renderScreen = () => {
+    switch (currentScreen) {
+      case "home":
+        return <HomeScreen onNavigate={setCurrentScreen} />;
+      case "request":
+        return <RequestScreen onBack={() => setCurrentScreen("home")} />;
+      case "collections":
+        return <CollectionsScreen onBack={() => setCurrentScreen("home")} />;
+      case "history":
+        return <HistoryScreen onBack={() => setCurrentScreen("home")} />;
+      default:
+        return <HomeScreen onNavigate={setCurrentScreen} />;
+    }
+  };
+
+  return (
+    <Box flexDirection="column">
+      {renderScreen()}
+    </Box>
+  );
+}
